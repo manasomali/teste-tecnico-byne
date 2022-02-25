96ec6dfd854850d877c90b03e679b14f19c92469
manasomali <matheus.marques_96@hotmail.com>
2022-02-25 01:28:21 -0300

    add type hints

--- utils.py
+++ utils.py
@@ -1,26 +1,27 @@
 import json
+from typing import Dict
 
 class DataModifier():
     def __init__(self, filename):
-        self.filename = filename
+        self.filename: str = filename
 
 
-    def getData(self):
+    def getData(self) -> Dict[str, int]:
         with open(self.filename, "r") as read_file:
-            data = json.load(read_file)
+            data: Dict[str, int] = json.load(read_file)
         
         return data
     
 
-    def addData(self, user):
-        data=self.getData()
+    def addData(self, user: str) -> None:
+        data: Dict[str, int] = self.getData()
         data[user] = 0
         with open(self.filename, "w+") as read_file:
             json.dump(data, read_file, indent=4)
 
 
-    def updateData(self, user, increment):
-        data=self.getData()
-        data[user] = data[user]+increment
+    def updateData(self, user: str, increment: int) -> None:
+        data: Dict[str, int] = self.getData()
+        data[user] = data[user] + increment
         with open(self.filename, "w+") as read_file:
             json.dump(data, read_file, indent=4)
