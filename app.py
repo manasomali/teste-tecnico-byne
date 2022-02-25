aa26af4c3ac3a3b4d4993ea2cd18e47ca0533ea4
manasomali <matheus.marques_96@hotmail.com>
2022-02-25 01:47:52 -0300

    Corrections with python linting

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
+import logging
+import requests
 from flask import Flask, render_template, redirect, request, session
 from apis import blueprint as api
-import requests
-import logging
 
 app = Flask(__name__)
 app.config['BASE_URL']='https://teste-tecnico-byne.herokuapp.com'
 #app.config['BASE_URL']='http://127.0.0.1:5000'
 app.config['TESTING'] = False
 app.config['DEBUG'] = False
-app.config['SECRET_KEY'] = '\xd91t\xfd_\xbb\xfc\x0b\xc2\xea\xcclg\x9f\xadu\xd1\xf6\xd9\xc5\x85f5\x17'
+app.config['SECRET_KEY']='\xd91t\xfd_\xbb\xfc\x0b\xc2\xea\xcclg\x9f\xadu\xd1\xf6\xd9\xc5\x85f5\x17'
 
 logging.basicConfig(format='%(asctime)s -> %(message)s', filename='logs.log', level=logging.WARNING)
 logging.getLogger('werkzeug').disabled = True
@@ -21,17 +21,17 @@ def index():
     if not session.get("name"):
         return redirect("/login")
 
-    return render_template('home.html', nome = session.get("name"), base_url = app.config['BASE_URL'])
+    return render_template('home.html', nome=session.get("name"), base_url=app.config['BASE_URL'])
 
 @app.route('/login', methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         if request.form.get("name") in getKeys():
             session["name"] = request.form.get("name")
-            logging.warning("Usuario {0} logado".format(session["name"]))
+            logging.warning("Usuario %s logado", session["name"])
             return redirect("/")
-        else:
-            return redirect("/registro")
+
+        return redirect("/registro")
     return render_template('login.html')
 
 @app.route('/registro', methods=["GET", "POST"])
@@ -39,27 +39,24 @@ def registro():
     if request.method == "POST":
         if request.form.get("name") in getKeys():
             return redirect("/login")
-        else:
-            session["name"] = request.form.get("name")
-            registraUser(request.form.get("name"))
-            logging.warning("Usuario {0} registrado".format(session["name"]))
-            return redirect("/")
+    
+        session["name"] = request.form.get("name")
+        registraUser(request.form.get("name"))
+        logging.warning("Usuario %s registrado", session["name"])
+        return redirect("/")
     return render_template('registro.html')
 
 @app.route('/logout', methods=["GET", "POST"])
 def logout():
-    logging.warning("Usuario {0} logout".format(session["name"]))
+    logging.warning("Usuario %s logout", session["name"])
     session.clear()
     return redirect("/")
-    
 
 def getKeys():
     return requests.get(app.config['BASE_URL']+'/api/registro').json()
 
-def registraUser(user):
+def registraUser(user: str):
     return requests.put(app.config['BASE_URL']+'/api/registro', data = {'user':user}).text
-
-
 
 if __name__ == '__main__':
     app.run()
