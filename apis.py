5cf2841aff4291da169dd8ecd1edac94360f7c06
manasomali <matheus.marques_96@hotmail.com>
2022-02-25 01:47:52 -0300

    correcoes com python linting

--- apis.py
+++ apis.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*- 
+import logging
+from random import randint
 from flask import Blueprint
 from flask_restx import Api, Resource, reqparse
-from random import randint
-import logging
 from utils import DataModifier
+
 logging.basicConfig(format='%(asctime)s -> %(message)s', filename='logs.log', level=logging.WARNING)
 
 blueprint = Blueprint('api', __name__)
@@ -19,38 +20,29 @@ parser.add_argument('increment', type=int, help='Informe incremento')
 @api.route('/impar', doc={"description": "Gera numero impar aletorio entre 0 e 99"})
 class Impar(Resource):
     def get(self):
-        try:
-            impar = 0
-            while (impar % 2) == 0:
-                impar = randint(0, 99)
-            
-            logging.warning("Numero impar requisitado -> {0}".format(impar))
-            return impar
-        except Exception as ex:
-            return "Ocorreu excecao do tipo {0}. Argumentos: {1!r}".format(type(ex).__name__, ex.args)
+        impar = 0
+        while (impar % 2) == 0:
+            impar = randint(0, 99)
+
+        logging.warning("Numero impar requisitado -> %s",impar)
+        return impar
 
 @api.route('/par', doc={"description": "Gera numero par aletorio entre 0 e 99"})
 class Par(Resource):
     def get(self):
-        try:
-            par = 1
-            while (par % 2) != 0:
-                par = randint(0, 99)
-            
-            logging.warning("Numero par requisitado ->  {0}".format(par))
-            return par
-        except Exception as ex:
-            return "Ocorreu excecao do tipo {0}. Argumentos: {1!r}".format(type(ex).__name__, ex.args)
+        par = 1
+        while (par % 2) != 0:
+            par = randint(0, 99)
+
+        logging.warning("Numero par requisitado ->  %s",par)
+        return par
 
 @api.route('/getvalorgeral', doc={"description": "Retorna valor geral de todos os users"})
 class GetValorGeral(Resource):
     def get(self):
-        try:
-            dados=database.getData()
-            logging.warning("Numero geral requisitado -> {0}".format(dados))
-            return dados
-        except Exception as ex:
-            return "Ocorreu excecao do tipo {0}. Argumentos: {1!r}".format(type(ex).__name__, ex.args)
+        dados=database.getData()
+        logging.warning("Numero geral requisitado -> %s",dados)
+        return dados
 
 @api.route('/putvalorgeral', doc={"description": "Incrementa valor de determinado user"})
 class PutValorGeral(Resource):
@@ -59,22 +51,17 @@ class PutValorGeral(Resource):
         user=parser.parse_args().get('user')
         increment=parser.parse_args().get('increment')
         database.updateData(user, increment)
-        logging.warning("Numero geral incrementado por {0} -> {1}".format(user,increment))
+        logging.warning("Numero geral incrementado por %s -> %s",user,increment)
         return database.getData()[user]
         
 @api.route('/registro', doc={"description": "Caso GET, retorna os users cadastrados e caso PUT registra um user novo"})
 class Registra(Resource):
     def get(self):
-        try:
-            return list(database.getData().keys())
-        except Exception as ex:
-            return "Ocorreu excecao do tipo {0}. Argumentos: {1!r}".format(type(ex).__name__, ex.args)
+        return list(database.getData().keys())
+
     @api.doc(params={'user': 'Nome do usuário para registro'})
     def put(self):
-        try:
-            user=parser.parse_args().get('user')
-            database.addData(user)
-            logging.warning("Usuário {0} registrado".format(user))
-            return 0
-        except Exception as ex:
-            return "Ocorreu excecao do tipo {0}. Argumentos: {1!r}".format(type(ex).__name__, ex.args)
+        user=parser.parse_args().get('user')
+        database.addData(user)
+        logging.warning("Usuário %s registrado",user)
+        return 0
--- utils.py
+++ utils.py
@@ -5,20 +5,17 @@ class DataModifier():
     def __init__(self, filename):
         self.filename: str = filename
 
-
     def getData(self) -> Dict[str, int]:
         with open(self.filename, "r") as read_file:
             data: Dict[str, int] = json.load(read_file)
-        
+
         return data
-    
 
     def addData(self, user: str) -> None:
         data: Dict[str, int] = self.getData()
         data[user] = 0
         with open(self.filename, "w+") as read_file:
             json.dump(data, read_file, indent=4)
-
 
     def updateData(self, user: str, increment: int) -> None:
         data: Dict[str, int] = self.getData()
