7c498c840607aef39f33564d5e4545368d64b598
manasomali <matheus.marques_96@hotmail.com>
2022-04-07 15:13:41 -0300

    commit to rodrigo see the problem

--- app.py
+++ app.py
@@ -1,67 +1,61 @@
 # -*- coding: utf-8 -*-
-import logging
 from flask import Flask, render_template, redirect, request, session
-from apis import blueprint as api
-from utils import getKeys, registerUser
+from utils import LoggingModifier, RequestsManager
 
-app = Flask(__name__)
-app.config['BASE_URL'] = 'https://teste-tecnico-byne.herokuapp.com'
-# app.config["BASE_URL"] = "http://127.0.0.1:5000"
-app.config["TESTING"] = False
-app.config["DEBUG"] = False
-app.config[
-    "SECRET_KEY"
-] = "\xd91t\xfd_\xbb\xfc\x0b\xc2\xea\xcclg\x9f\xadu\xd1\xf6\xd9\xc5\x85f5\x17"
+class Application:
+    def __init__(self, base_url="http://localhost:8000/", testing=False, debuging=False):
+        self.application = Flask(__name__)
+        self.application.config["BASE_URL"] = base_url
+        self.application.config["TESTING"] = testing
+        self.application.config["DEBUG"] = debuging
+        self.application.config[
+            "SECRET_KEY"
+        ] = "\xd91t\xfd_\xbb\xfc\x0b\xc2\xea\xcclg\x9f\xadu\xd1\xf6\xd9\xc5\x85f5\x17"
 
-logging.basicConfig(
-    format="%(asctime)s -> %(message)s", filename="logs.log", level=logging.WARNING
-)
-logging.getLogger("werkzeug").disabled = True
+    def run(self):
+        self.application.run(host=self.application.config["BASE_URL"])
+        
 
-app.register_blueprint(api, url_prefix="/api")
+    def register_blueprint(self, api, prefix):
+        self.application.register_blueprint(api, url_prefix=prefix)
 
+    def setup_routes(self, log_mod: LoggingModifier, req_man: RequestsManager):
+        @self.application.route("/", methods=["GET", "POST"])
+        def index():
+            if not session.get("name"):
+                return redirect("/login")
 
-@app.route("/", methods=["GET", "POST"])
-def index():
-    if not session.get("name"):
-        return redirect("/login")
+            return render_template(
+                "home.html",
+                nome=session.get("name"),
+                base_url=self.application.config["BASE_URL"],
+            )
 
-    return render_template(
-        "home.html", nome=session.get("name"), base_url=app.config["BASE_URL"]
-    )
+        @self.application.route("/login", methods=["GET", "POST"])
+        def login():
+            if request.method == "POST":
+                if request.form.get("name") in req_man.get_users_keys():
+                    session["name"] = request.form.get("name")
+                    log_mod.write_message("User {} login successfully".format(session["name"]))
+                    return redirect("/")
 
+                return redirect("/register")
+            return render_template("login.html")
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        if request.form.get("name") in getKeys(app):
-            session["name"] = request.form.get("name")
-            logging.warning("Usuario %s logado", session["name"])
+        @self.application.route("/register", methods=["GET", "POST"])
+        def register():
+            if request.method == "POST":
+                if request.form.get("name") in req_man.get_users_keys():
+                    return redirect("/login")
+
+                session["name"] = request.form.get("name")
+                req_man.register_user(request.form.get("name"))
+                log_mod.write_message("User {} registred".format(session["name"]))
+                return redirect("/")
+            return render_template("register.html")
+
+        @self.application.route("/logout", methods=["GET", "POST"])
+        def logout():
+            log_mod.write_message("User {} logout".format(session["name"]))
+            session.clear()
             return redirect("/")
-
-        return redirect("/register")
-    return render_template("login.html")
-
-
-@app.route("/register", methods=["GET", "POST"])
-def register():
-    if request.method == "POST":
-        if request.form.get("name") in getKeys(app):
-            return redirect("/login")
-
-        session["name"] = request.form.get("name")
-        registerUser(app, request.form.get("name"))
-        logging.warning("User %s registred", session["name"])
-        return redirect("/")
-    return render_template("register.html")
-
-
-@app.route("/logout", methods=["GET", "POST"])
-def logout():
-    logging.warning("User %s logout", session["name"])
-    session.clear()
-    return redirect("/")
-
-
-if __name__ == "__main__":
-    app.run()
--- utils.py
+++ utils.py
@@ -1,36 +1,52 @@
 import json
-import requests
 from typing import Dict
-
+import logging
+import requests
 
 class DataModifier:
     def __init__(self, filename):
         self.filename: str = filename
 
-    def getData(self) -> Dict[str, int]:
+    def get_data(self) -> Dict[str, int]:
         with open(self.filename, "r") as read_file:
             data: Dict[str, int] = json.load(read_file)
 
         return data
 
-    def addData(self, user: str) -> None:
-        data: Dict[str, int] = self.getData()
+    def add_data(self, user: str) -> None:
+        data: Dict[str, int] = self.get_data()
         data[user] = 0
         with open(self.filename, "w+") as read_file:
             json.dump(data, read_file, indent=4)
 
-    def updateData(self, user: str, increment: int) -> None:
-        data: Dict[str, int] = self.getData()
+    def update_data(self, user: str, increment: int) -> None:
+        data: Dict[str, int] = self.get_data()
         data[user] = data[user] + increment
         with open(self.filename, "w+") as read_file:
             json.dump(data, read_file, indent=4)
 
 
-def getKeys(app):
-    return requests.get(app.config["BASE_URL"] + "/api/register").json()
+class LoggingModifier:
+    def __init__(self, file_name, disable_werkzeug=True) -> None:
+        logging.basicConfig(
+            format="%(asctime)s -> %(message)s",
+            filename=file_name,
+            level=logging.WARNING,
+        )
+        logging.getLogger("werkzeug").disabled = disable_werkzeug
+
+    def write_message(self, message) -> None:
+        logging.warning(message)
 
 
-def registerUser(app, user: str):
-    return requests.put(
-        app.config["BASE_URL"] + "/api/register", data={"user": user}
-    ).text
+class RequestsManager:
+    def __init__(self, base_url="http://localhost:8000/") -> None:
+        self.base_url = base_url
+
+    def get_users_keys(self):
+        return requests.get(self.base_url + "/api/register").json()
+
+    def register_user(self, user: str):
+        return requests.put(
+            self.base_url + "/api/register", data={"user": user}
+        ).text
