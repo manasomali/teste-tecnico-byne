a80ed027e056e5411c920c20e44ad7516db05524
manasomali <matheus.marques_96@hotmail.com>
2022-04-07 17:36:45 -0300

    add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from ensurepip import version
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="teste-tecnico-byne",
+    version="0",
+    author="Matheus Nascimento S. M. de Lima",
+    author_email="matheus.marques_96@hotmail.com",
+    packages=find_packages(),
+    install_requires=["Flask",
+        "Click",
+        "gunicorn",
+        "itsdangerous",
+        "Jinja2",
+        "Werkzeug",
+        "flask-restx",
+        "requests",
+        "MarkupSafe",
+        "pip",
+        "setuptools"
+    ]
+)
